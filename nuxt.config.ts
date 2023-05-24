676be846aa49d9073ba0b6430b64f736720ad809
Jordi Gasau <50402221+jordigasau@users.noreply.github.com>
2023-05-24 19:47:32 +0200

    Canvi a spa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr:true // Server Side Rendering (static)
+    ssr:false // Server Side Rendering (static)
     //ssr:false // No Server Side Rendering (spa)
 })
